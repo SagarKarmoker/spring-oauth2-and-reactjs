d9f1f68bd35ea5b96131e372ed8dcbecce8f7119
Sagar Karmoker <sagarkarmoker2001@gmail.com>
2024-09-30 14:14:24 +0600

    backend: using spring security Oauth2

---
+++ backend/src/main/java/com/sagar/SpringSecurityAndReact/SpringSecurityAndReactApplication.java
@@ -0,0 +1,13 @@
+package com.sagar.SpringSecurityAndReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAndReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAndReactApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sagar/SpringSecurityAndReact/SpringSecurityAndReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagar.SpringSecurityAndReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAndReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
