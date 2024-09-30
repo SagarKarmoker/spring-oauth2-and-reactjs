f2b11120fc7d119e68f12a57a0433de2a87f563d
Sagar Karmoker <sagarkarmoker2001@gmail.com>
2024-09-30 16:56:16 +0600

    frontend: completed

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,7 +1,45 @@
-// import React from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react"
 
 export default function Dashboard() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/user", {
+          withCredentials: true
+        })
+  
+        if (response.status === 200) {
+          setUser(response.data)
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    fetchUser();
+  }, [])
+
   return (
-    <div>Dashboard</div>
+    <div>
+      <h3>Dashboard</h3>
+      {
+        user ? (
+          <div>
+            <p>Name: {user.name} </p>
+            <p>Email: {user.email}</p>
+            <div>
+              <img src={user?.picture} />
+            </div>
+          </div>
+        ) : (
+          <div>
+            Loading user data
+          </div>
+        )
+      }
+    </div>
   )
 }
